9f02465611def75d8d67e001d44adfadeea94723
Steve Schwedt <sschwedt@gmail.com>
2020-01-18 15:57:09 -0700

    Shows widgets to logged in users

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,3 +1,5 @@
 class Widget < ApplicationRecord
   belongs_to :user
+
+  enum color: %i[red blue green yellow purple]
 end
--- spec/features/widget_index_spec.rb
+++ spec/features/widget_index_spec.rb
@@ -5,6 +5,22 @@ describe 'widget index' do
     visit(widgets_path)
 
     expect(current_path).to eq(root_path)
-    expect(page).to have_content("Access Denied")
+    expect(page).to have_content('Access Denied')
+  end
+
+  it 'shows widgets to logged in user' do
+    bob = login_bob
+    widget1 = Widget.create(name: 'wow', description: 'yowza', color: 'red', picture: 'http://www.cat.com/pic', is_public: true, user: bob)
+
+    visit(widgets_path)
+
+    expect(current_path).to eq(widgets_path)
+    expect(page).to_not have_content('Access Denied')
+
+    expect(page).to have_content('wow')
+    expect(page).to have_content('yowza')
+    expect(page).to have_content('red')
+    expect(page).to have_content('true')
+    expect(page).to have_content('Bob')
   end
 end
