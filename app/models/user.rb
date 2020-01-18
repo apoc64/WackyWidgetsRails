9646e4c14294c9a7fa3279c32e84d08ba3eea628
Steve Schwedt <sschwedt@gmail.com>
2020-01-18 11:20:45 -0700

    Requires strong passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   validates_presence_of :email
   validates_uniqueness_of :email
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+    errors.add :password, 'Password must be 8-70 characters, and contain at least one lowercase, uppercase, number, and special character'
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,31 @@ require 'rails_helper'
 describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of (:email) }
+
+    it 'only saves users with strong passwords' do
+      user = User.create(email: 'bob@bob.bob', password: '1234')
+      expect(user.persisted?).to eq(false)
+
+      user = User.create(email: 'bob@bob.bob', password: '1234abcd')
+      expect(user.persisted?).to eq(false)
+
+      user = User.create(email: 'bob@bob.bob', password: '1234Abcd')
+      expect(user.persisted?).to eq(false)
+
+      user = User.create(email: 'bob@bob.bob', password: '1234Abcdef')
+      expect(user.persisted?).to eq(false)
+
+      user = User.create(email: 'bob@bob.bob', password: '123456789!')
+      expect(user.persisted?).to eq(false)
+
+      user = User.create(email: 'bob@bob.bob', password: 'qwertyuio!')
+      expect(user.persisted?).to eq(false)
+
+      expect(User.all.count).to eq(0)
+
+      user = User.create(email: 'bob@bob.bob', password: '1234Abc!')
+      expect(user.persisted?).to eq(true)
+      expect(User.all.count).to eq(1)
+    end
   end
 end
