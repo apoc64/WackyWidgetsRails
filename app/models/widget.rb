81f27a127e5d45b38260ac1aac47e8643897be8b
Steve Schwedt <sschwedt@gmail.com>
2020-03-21 17:09:03 -0600

    Uploads widgets to s3

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -9,4 +9,19 @@ class Widget < ApplicationRecord
   def self.color_options
     colors.keys
   end
+
+  def self.publish
+    file_name = 'widgets.json'
+    data = all.to_json
+    file = Tempfile.new(file_name)
+    file.write(data)
+    file.rewind
+    file.close
+
+    s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+    bucket = s3.bucket(ENV['AWS_BUCKET'])
+
+    object = bucket.object(file_name)
+    object.upload_file(file.path)
+  end
 end
